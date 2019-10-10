e3d199424f2cd2772945a8de5e6e9d56b92e2812
Josh Parry <josh.parry97@gmail.com>
2019-10-10 20:53:42 +1100

    finshed hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Nav from './nav.jsx';
+import './nav.css';
+import SideDrawer from './SideDraw/SideDraw.jsx';
+import Backdrop from './BackDrop/BackDrop'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    sideDrawerOpen: false
+  }
+
+  drawerToggleClickHandler = () => {
+    
+    // Not best practice.
+    // this.setState({sideDrawerOpen: !this.state.sideDrawerOpen})
+    // Use function form instead
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen}
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  render() {
+
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      // sideDrawer = <SideDrawer />
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+
+    return (
+      // edited index.css for body and html height 100% 
+      <div style={{ height: '100%'}}>
+        <Nav drawerClickHandler={this.drawerToggleClickHandler}/>
+        
+        <SideDrawer show={this.state.sideDrawerOpen}/>
+        {backdrop}
+  
+      <main style={{ marginTop: '10vh'}}>
+        <p>This is the page content</p> 
+      </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/nav.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+import DrawerToggleButton from './SideDraw/DrawerToggleButton';
+
+const nav = props => (
+    <header className="nav">
+        <nav className="navbar_navigation">
+            <div className="nav-toggle-btn">
+                <DrawerToggleButton click={props.drawerClickHandler} />
+            </div>
+            <div className="nav-logo"><a href="/">THE LOGO</a></div>
+            {/* spacer takes all the available space */}
+            <div className="spacer"></div>
+            <div className="nav-items">
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                    <li><a href="/">Signin</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default nav;
